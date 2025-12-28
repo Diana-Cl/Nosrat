f46c0d099b9b9f510cacd462044c06a8a917c791
Diana <135276904+Diana-Cl@users.noreply.github.com>
2025-12-28 19:42:57 +0330

    Paused: Unforseen war

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,11 @@
 import DefaultTheme from 'vitepress/theme';
 import type { Theme } from 'vitepress';
 import './custom.css';
+import AudioPlayer from './components/AudioPlayer.vue';
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
-    DefaultTheme.enhanceApp?.({ app });
+    app.component('AudioPlayer', AudioPlayer);
   },
 } satisfies Theme;
